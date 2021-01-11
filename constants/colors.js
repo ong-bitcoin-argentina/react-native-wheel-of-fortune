2b86edbcd8103b1ac63089827fd9bea2c53ae833
jmartel-atixlabs <jmartel@atixlabs.com>
2021-01-11 08:29:42 -0300

    refactor: colors constants

---
+++ constants/colors.js
@@ -0,0 +1,27 @@
+export const refenceColors = {
+    "#417FD7" : "#B89AED",
+    "#69A7FE" : "#906ECD",
+    "#E03C7A" : "#FFDE6D",
+    "#F97EAD" : "#FFC500",
+    "#06CBB5" : "#92ED86",
+    "#5CE6D6" : "#6ECC62",
+    "#6ECC62" : "#5CE6D6",
+    "#92ED86" : "#06CBB5",
+    "#FFC500" : "#F97EAD",
+    "#FFDE6D" : "#E03C7A",
+    "#906ECD" : "#69A7FE",
+    "#B89AED" : "#417FD7",
+};
+
+export const defaultColors = [
+    "#E07026",
+    "#E8C22E",
+    "#ABC937",
+    "#4F991D",
+    "#22AFD3",
+    "#5858D0",
+    "#7B48C8",
+    "#D843B9",
+    "#E23B80",
+    "#D82B2B",
+  ]
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import {
   Image as RNImage,
 } from "react-native";
 import * as d3Shape from "d3-shape";
+import {defaultColors, refenceColors} from '../constants/colors';
 
 import Svg, {
   G,
@@ -23,20 +24,7 @@ const AnimatedSvg = Animated.createAnimatedComponent(Svg);
 
 const { width, height } = Dimensions.get("screen");
 
-const colors = {
-  "#417FD7" : "#B89AED",
-  "#69A7FE" : "#906ECD",
-  "#E03C7A" : "#FFDE6D",
-  "#F97EAD" : "#FFC500",
-  "#06CBB5" : "#92ED86",
-  "#5CE6D6" : "#6ECC62",
-  "#6ECC62" : "#5CE6D6",
-  "#92ED86" : "#06CBB5",
-  "#FFC500" : "#F97EAD",
-  "#FFDE6D" : "#E03C7A",
-  "#906ECD" : "#69A7FE",
-  "#B89AED" : "#417FD7",
-};
+const colors = refenceColors;
 
 class WheelOfFortune extends Component {
   constructor(props) {
@@ -94,18 +82,7 @@ class WheelOfFortune extends Component {
     const arcs = d3Shape.pie()(data);
     var colors = this.props.colors
       ? this.props.colors
-      : [
-          "#E07026",
-          "#E8C22E",
-          "#ABC937",
-          "#4F991D",
-          "#22AFD3",
-          "#5858D0",
-          "#7B48C8",
-          "#D843B9",
-          "#E23B80",
-          "#D82B2B",
-        ];
+      : defaultColors;
     return arcs.map((arc, index) => {
       const instance = d3Shape
         .arc()
